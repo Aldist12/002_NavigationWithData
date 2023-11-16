fa0fc7e3369c7f92d5ae54d762508d58603a6e51
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2023-11-16 18:41:20 +0700

    Add Function HalamanForm

--- app/src/main/java/com/example/myappesjumbo/HalamanForm.kt
+++ app/src/main/java/com/example/myappesjumbo/HalamanForm.kt
@@ -1,2 +1,12 @@
 package com.example.myappesjumbo
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HalamanForm(
+    onSubmitBUttonClicked: (MutableList<String>) -> Unit,
+    onCancelButtonClicked: () -> Unit,
+
+    ) {
+
+}
