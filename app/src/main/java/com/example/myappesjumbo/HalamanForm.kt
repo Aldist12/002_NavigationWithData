b1456bb7a997e0aca1d8d887bc91bc6b83c023c9
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2023-11-16 18:48:25 +0700

    Add Var & Column

--- app/src/main/java/com/example/myappesjumbo/HalamanForm.kt
+++ app/src/main/java/com/example/myappesjumbo/HalamanForm.kt
@@ -1,6 +1,17 @@
 package com.example.myappesjumbo
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HalamanForm(
@@ -8,5 +19,17 @@ fun HalamanForm(
     onCancelButtonClicked: () -> Unit,
 
     ) {
+    var nama by rememberSaveable { mutableStateOf("") }
+    var noHp by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var listData: MutableList<String> = mutableListOf(nama, noHp, alamat)
 
-}
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Data Pelanggan")
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
