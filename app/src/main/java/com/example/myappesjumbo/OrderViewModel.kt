83282ed9ee465fae827aef50968e11701b060666
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2023-11-16 19:02:11 +0700

    Fix code OrderViewModel

--- app/src/main/java/com/example/myappesjumbo/OrderViewModel.kt
+++ app/src/main/java/com/example/myappesjumbo/OrderViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.myappesjumbo
 
-import com.example.myappesjumbo.data.OrderUIState
 import androidx.lifecycle.ViewModel
+import com.example.myappesjumbo.data.OrderUIState
+//import com.example.myappesjumbo.data.harga
+//import com.example.myappesjumbo.data.rasa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -9,32 +11,45 @@ import kotlinx.coroutines.flow.update
 import java.text.NumberFormat
 
 private const val HARGA_PER_CUP = 3000
-
-class OrderViewModel : ViewModel() {
+class OrderViewModel : ViewModel(){
     private val _stateUI = MutableStateFlow(OrderUIState())
-    val stateUI: StateFlow<OrderUIState> = _stateUI.asStateFlow()
+    val stateUI:StateFlow<OrderUIState> = _stateUI.asStateFlow()
 
-    fun setJumlah(jmlEsJumbo: Int) {
+    fun setJumlah(jmlESTEHJUMBO:Int){
         _stateUI.update { stateSaatIni ->
             stateSaatIni.copy(
-                jumlah = jmlEsJumbo,
-                harga = hitungHarga(jumlah = jmlEsJumbo)
+                jumlah = jmlESTEHJUMBO,
+                harga = hitungHarga(jumlah = jmlESTEHJUMBO)
+            )
+        }
+    }
+    fun setRasa(rasaPilihan: String){
+        _stateUI.update { stateSaatIni ->
+            stateSaatIni.copy(rasa = rasaPilihan)
+        }
+    }
+    fun  resetOrder(){
+        _stateUI.value = OrderUIState()
+    }
+    fun resetForm(){
+        _stateUI.value = OrderUIState()
+    }
+
+    private fun hitungHarga(
+        jumlah : Int =_stateUI.value.jumlah,
+    ): String{
+        val kalkulasiHarga = jumlah * HARGA_PER_CUP
+
+        return NumberFormat.getNumberInstance().format(kalkulasiHarga)
+    }
+    fun setContact(listContact: MutableList<String>) {
+        _stateUI.update { stateSaatIni ->
+            stateSaatIni.copy(
+                name = listContact[0],
+                noTelp = listContact[1],
+                alamat = listContact[2]
             )
         }
     }
 
-    fun setRasa(rasaPilihan: String) {
-        _stateUI.update { stateSaatIni -> stateSaatIni.copy(rasa = rasaPilihan) }
-
-    }
-
-    fun resetOrder() {
-        _stateUI.value = OrderUIState()
-    }
-
-    private fun hitungHarga(jumlah: Int = _stateUI.value.jumlah): String {
-        val kalkulasiHarga = jumlah * HARGA_PER_CUP
-
-        return NumberFormat.getInstance().format(kalkulasiHarga)
-    }
 }
